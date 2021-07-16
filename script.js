30531bc88e7a0c6ace2a03e204e17e3f687faa7a
pramodtarpe <72032932+pramodtarpe@users.noreply.github.com>
2021-07-16 20:01:04 +0530

    add syntax error check

--- script.js
+++ script.js
@@ -2,20 +2,34 @@ var buttons = document.getElementsByClassName('button');
 var display = document.getElementById('expression');
 
 display.textContent = 0;
+function isOperator(op){
+    return (op == "." || op == "+" || op == "-" || op == "*" || op == "/");
+}
 function process(){
     var value = this.getAttribute('data-key');
     value = value.trim();
-    if(display.textContent == "0"){
-        display.textContent = "";
+    if(display.textContent == "0" && value == "."){
+        display.textContent = "0.";
     }
-    if(value == "="){
-        var result = eval(display.textContent);
-        display.textContent = result;
+    else if(value == "="){
+        try{
+            var result = eval(display.textContent);
+            display.textContent = result;
+        }
+        catch(err){
+            display.textContent = "SYNTAX ERROR";
+        }
     }
     else if(value == "AC"){
         display.textContent = "0";
     }
+    else if(isOperator(value) && display.textContent[display.textContent.length-1] == value){
+        display.textContent = display.textContent; 
+    }
     else{
+        if(display.textContent == "0"){
+            display.textContent = "";
+        }
         display.textContent += value;
     }
 }

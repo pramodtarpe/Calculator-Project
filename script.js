c27fc7423fea2d6043cfb955db9de4d6076441d6
pramodtarpe <72032932+pramodtarpe@users.noreply.github.com>
2021-07-23 03:10:34 -0700

    animate on keypress

--- script.js
+++ script.js
@@ -5,65 +5,62 @@ display.textContent = 0;
 function isOperator(op){
     return (op == "." || op == "+" || op == "-" || op == "*" || op == "/" || op == "%");
 }
-function process(){
-    var value = this.getAttribute('data-key');
-    value = value.trim();
-    if(display.textContent == "0" && value == "."){
-        display.textContent = "0.";
+function calculate(value){
+    //add animation
+    if(value == "13"){
+        value = "=";
     }
-    else if(value == "="){
-        try{
-            var result = eval(display.textContent);
-            display.textContent = result;
+    if(value == "c"){
+        value = "C";
+    }
+    let btn = document.getElementById("btn-" + value.charCodeAt());
+    if(btn != null){
+        btn.classList.add('animate-btn');
+        setTimeout(() => {
+            btn.classList.remove('animate-btn');
+        }, 100);
+        //perform keypress or click
+        if(display.textContent == "0" && value == "."){
+            display.textContent = "0.";
         }
-        catch(err){
-            display.textContent = "SYNTAX ERROR";
+        else if(value == "="){
+            try{
+                var result = eval(display.textContent);
+                display.textContent = result;
+            }
+            catch(err){
+                display.textContent = "SYNTAX ERROR";
+            }
         }
-    }
-    else if(value == "AC"){
-        display.textContent = "0";
-    }
-    else if(isOperator(value) && display.textContent[display.textContent.length-1] == value){
-        display.textContent = display.textContent; 
-    }
-    else{
-        if(display.textContent == "0"){
-            display.textContent = "";
+        else if(value == "C" || value == "c"){
+            display.textContent = "0";
         }
-        display.textContent += value;
+        else if(isOperator(value) && display.textContent[display.textContent.length-1] == value){
+            display.textContent = display.textContent; 
+        }
+        else{
+            if(display.textContent == "0"){
+                display.textContent = "";
+            }
+            display.textContent += value;
+        }
     }
 }
+function process(element){
+    var value = element.target.getAttribute('data-key');
+    calculate(value);
+}
 function keyProcess(){
-    var value = String.fromCharCode(event.keyCode);
-    value = value.trim();
-    if(display.textContent == "0" && value == "."){
-        display.textContent = "0.";
+    var value = event.keyCode;
+    if(value != "13"){
+        value = String.fromCharCode(value);
     }
-    else if(value == "=" || event.keyCode == 13){
-        try{
-            var result = eval(display.textContent);
-            display.textContent = result;
-        }
-        catch(err){
-            display.textContent = "SYNTAX ERROR";
-        }
-    }
-    else if(value == "AC"){
-        display.textContent = "0";
-    }
-    else if(isOperator(value) && display.textContent[display.textContent.length-1] == value){
-        display.textContent = display.textContent; 
-    }
-    else if((value >= "0" && value <= "9") || ((value == "." || value == "+" || value == "-" || value == "*" || value == "/" || value == "%" || value == "(" || value == ")"))){
-        if(display.textContent == "0"){
-            display.textContent = "";
-        }
-        display.textContent += value;
-    }
+    calculate(value);
 }
 // Add event listener to all buttons
 for(let i = 0;i<buttons.length;i++){
-    buttons[i].addEventListener("click",process,false);
+    buttons[i].addEventListener("mousedown",process);
+    buttons[i].id = "btn-" + (buttons[i].textContent).charCodeAt();
 }
 
 document.addEventListener("keypress",keyProcess);
